b624a9ec7b90c89bd0300bc9b5f91a28ae719ee1
Matteo Cusinato <akymos@users.noreply.github.com>
2022-08-13 18:17:19 +0200

    add promptui to delete

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,10 +1,17 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/akymos/ff/internal"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
+
+type aliasList struct {
+	Key string
+	Val string
+}
 
 var (
 	deleteCmd = &cobra.Command{
@@ -14,17 +21,59 @@ var (
 		Long: `Delete a directory alias.
 
 Arguments:
-[alias] required. The name of the alias.`,
-		Args: cobra.ExactArgs(1),
+[alias] optional. The name of the alias.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			alias := args[0]
-			err := internal.LocalDb.Del(alias)
-			if err != nil {
-				return err
-			}
-			err = internal.PopulateAlias()
-			if err != nil {
-				return err
+			alias := ""
+			if len(args) < 1 {
+				list := internal.LocalDb.FindAll()
+				if len(list) == 0 {
+					return errors.New("no aliases found")
+				}
+				aliasesList := make([]aliasList, 0)
+				for k, v := range list {
+					aliasesList = append(aliasesList, aliasList{Key: k, Val: v})
+				}
+				aliasesList = append(aliasesList, aliasList{Key: "Exit", Val: "No delete any alias"})
+
+				prompt := promptui.Select{
+					Label: "Select an alias to delete:",
+					Items: aliasesList,
+					Templates: &promptui.SelectTemplates{
+						Label:    "{{ . }}",
+						Active:   "-> {{ .Key }} ({{ .Val }})",
+						Inactive: "{{ .Key }} ({{ .Val }})",
+						Selected: "-> {{ .Key }} ({{ .Val }})",
+					},
+				}
+
+				i, _, err := prompt.Run()
+
+				if err != nil {
+					return err
+				}
+
+				if i == len(aliasesList)-1 {
+					return nil
+				}
+				alias = aliasesList[i].Key
+				err = internal.LocalDb.Del(alias)
+				if err != nil {
+					return err
+				}
+				err = internal.PopulateAlias()
+				if err != nil {
+					return err
+				}
+			} else {
+				alias = args[0]
+				err := internal.LocalDb.Del(alias)
+				if err != nil {
+					return err
+				}
+				err = internal.PopulateAlias()
+				if err != nil {
+					return err
+				}
 			}
 			internal.WriteDb()
 			fmt.Printf("Alias %s deleted.\n", alias)
